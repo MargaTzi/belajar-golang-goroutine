85d3cc8e3fa3e16cc68ea756f5df655c27c57cb0
cariomadaardisto <cariomadaardisto@gmail.com>
2025-11-03 20:45:43 +0700

    up

---
+++ helper/timer_test.go
@@ -0,0 +1,44 @@
+package helper
+
+import (
+	"fmt"
+	"sync"
+	"testing"
+	"time"
+)
+
+func TestTimer(t *testing.T) {
+	timer := time.NewTimer(5 * time.Second)
+	fmt.Println(time.Now())
+
+	time := <-timer.C
+	fmt.Println(time)
+}
+
+func TestAfter(t *testing.T) {
+	channel := time.After(1 * time.Second)
+
+	tick := <-channel
+	fmt.Println(tick)
+}
+
+func TestAfterFunc(t *testing.T) {
+	group := sync.WaitGroup{}
+	group.Add(1)
+
+	time.AfterFunc(1*time.Second, func() {
+		fmt.Println("execute after 1 second")
+		group.Done()
+	})
+
+	group.Wait()
+}
+
+func TestTicker(t *testing.T) {
+	ticker := time.NewTicker(1 * time.Second)
+	defer ticker.Stop()
+
+	for tick := range ticker.C {
+		fmt.Println(tick)
+	}
+}
